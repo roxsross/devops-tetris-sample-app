b9827eb95453d97dba827e347c7cc353bae56075
Rossana Suarez <46635001+roxsross@users.noreply.github.com>
2024-01-09 21:10:49 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "./index.css";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+
+// Envuelve tu componente principal con BrowserRouter y establece la ruta base
+ReactDOM.render(
+  <Router basename="/">
+    <App />
+  </Router>,
+  rootElement
+);
